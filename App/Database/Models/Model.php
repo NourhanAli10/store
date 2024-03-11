229a4e96fc904c7b29ae671205ad13967281c207
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-11 23:05:25 +0200

    tasks

---
+++ App/Database/Models/Model.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Database\Models;
+
+use App\Database\Config\Connection;
+
+class Model extends Connection {
+    public function find()
+    {
+        # code...
+    }
+    public function all()
+    {
+        # code...
+    }
+}
+
+
